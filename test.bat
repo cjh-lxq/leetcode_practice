f3c7eea81002ecb1c1b6d689f443486d0c9a7c15
cjh-lxq <18601277500@163.com>
2018-11-25 19:02:48 +0800

    2018-11-2519:02:47

--- test.bat
+++ test.bat
@@ -1,6 +1,8 @@
+set filename=%date:~0,4%-%date:~5,2%-%date:~8,2%%time:~0,2%:%time:~3,2%:%time:~6,2%
+echo %filename%
 git remote -v
 git add -A
 git status
-git commit -m "auto commit"
+git commit -m %filename%
 git push github computer_alienware
 pause
