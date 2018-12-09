21d08a8b3267773d61c36cda0ebefcc4d383bceb
cjhepsilondelta <cjh18601277500@163.com>
2018-12-09 09:16:54 +0800

    2018-12-09-9:16:54

--- auto_update.bat
+++ auto_update.bat
@@ -5,8 +5,9 @@ set filename=%date:~0,4%-%date:~5,2%-%date:~8,2%-%time:~1,2%%time:~3,2%:%time:~6
 set filename=%date:~0,4%-%date:~5,2%-%date:~8,2%-%time:~0,2%:%time:~3,2%:%time:~6,2%
 )
 echo.
-echo "Time: "%filename%
+echo Time: %filename%
 git remote -v
+git branch
 git add -A
 git status
 git commit -m %filename%
