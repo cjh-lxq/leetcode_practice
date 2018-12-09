fc5c67e1e8f23e2829c97cb7318a0149ae7eb529
cjhepsilondelta <cjh18601277500@163.com>
2018-12-09 09:17:41 +0800

    2018-12-09-9:17:41

--- auto_update.bat
+++ auto_update.bat
@@ -4,7 +4,7 @@ set filename=%date:~0,4%-%date:~5,2%-%date:~8,2%-%time:~1,2%%time:~3,2%:%time:~6
 ) else (
 set filename=%date:~0,4%-%date:~5,2%-%date:~8,2%-%time:~0,2%:%time:~3,2%:%time:~6,2%
 )
-echo.
+@echo on
 echo Time: %filename%
 git remote -v
 git branch
