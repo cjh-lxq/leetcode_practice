0a6facc479fc5f9de4230f93a54762362a2aead0
cjhepsilondelta <cjh18601277500@163.com>
2018-12-09 09:16:03 +0800

    2018-12-09-9:16:02

--- auto_update.bat
+++ auto_update.bat
@@ -1,10 +1,11 @@
 @echo off
-set filename=%date:~0,4%-%date:~5,2%-%date:~8,2%-%time:~0,2%:%time:~3,2%:%time:~6,2%
 if %time:~0,2% LSS 10 (
 set filename=%date:~0,4%-%date:~5,2%-%date:~8,2%-%time:~1,2%%time:~3,2%:%time:~6,2%
+) else (
+set filename=%date:~0,4%-%date:~5,2%-%date:~8,2%-%time:~0,2%:%time:~3,2%:%time:~6,2%
 )
 echo.
-echo %filename%
+echo "Time: "%filename%
 git remote -v
 git add -A
 git status
